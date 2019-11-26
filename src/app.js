2e8e56535ae2aa1e54e7c5480ce7b486e63f6b2f
Demeco <Demeco_Bell@demecos-mbp.attlocal.net>
2019-11-25 23:16:27 -0500

    setup express server, install babel

---
+++ src/app.js
@@ -0,0 +1,21 @@
+import express from 'express';
+import path from 'path';
+import ejs from 'ejs';
+import bodyParser from 'body-parser';
+import axios from 'axios';
+const app = express();
+
+const port = process.env.PORT || 8080;
+
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json());
+
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(__dirname + '/views'))
+
+
+
+
+
+app.listen(port, () => console.log('server is running'));
